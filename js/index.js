ce7670169c5513ca5b55a1a97338022bbd2baa26
Prasad Lokhande <developerpra21@gmail.com>
2023-09-01 03:12:17 +0530

    animation completed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const tl = gsap.timeline();
+const tl = gsap.timeline({ repeat: 5, repeatDelay: 2 });
 // tl.from(".overlay", {
 //   width: 0,
 //   top: 0,
@@ -26,8 +26,66 @@ tl.from(
 tl.from(
   ".box3",
   {
-    autoAlpha: 0,
     y: -200,
+  },
+  0.75
+);
+tl.from(
+  ".box4",
+  {
+    autoAlpha: 0,
+    scale: 2,
+    x: 400,
+    duration: 0.6,
+  },
+  0.8
+);
+tl.from(
+  ".box5",
+  {
+    y: 400,
+  },
+  0.75
+);
+tl.from(
+  ".box6",
+  {
+    y: 100,
+  },
+  0.75
+);
+tl.from(
+  ".box7",
+  {
+    x: -200,
+  },
+  0.75
+);
+tl.from(
+  ".box8",
+  {
+    y: 100,
+  },
+  0.75
+);
+tl.from(
+  ".box9",
+  {
+    x: 100,
+  },
+  0.75
+);
+tl.from(
+  ".arrow1",
+  {
+    autoAlpha: 0,
+  },
+  0.75
+);
+tl.from(
+  ".downArrow1",
+  {
+    scale: 0,
   },
   0.75
 );
