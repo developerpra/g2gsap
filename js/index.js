340351574ce58ae82f86ca894970a16a37fc6991
Prasad Lokhande <developerpra21@gmail.com>
2023-09-01 21:17:07 +0530

    fonts udpated and stable version before clamp

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-const tl = gsap.timeline({ repeat: 5, repeatDelay: 2 });
-// tl.from(".overlay", {
-//   width: 0,
-//   top: 0,
-//   right: 0,
-// });
-// tl.to(".overlay", {
-//   width: 0,
-//   left: 0,
-// });
+const tl = gsap.timeline();
+tl.to(".overlay", {
+  delay: 1,
+  duration: 0.5,
+});
+tl.to(".overlay", {
+  width: 0,
+  top: 0,
+  right: 0,
+  duration: 0.5,
+});
+
 tl.from(".mainbox", {
   width: 0,
   delay: 0.5,
@@ -16,20 +18,23 @@ tl.from(".mainbox", {
 tl.from(".content", {
   autoAlpha: 0,
 });
+
 tl.from(
   ".box2",
   {
     x: -400,
   },
-  "<-0.5"
+  ">-1"
 );
+
 tl.from(
   ".box3",
   {
-    y: -200,
+    y: -300,
   },
-  0.75
+  2.75
 );
+
 tl.from(
   ".box4",
   {
@@ -38,54 +43,56 @@ tl.from(
     x: 400,
     duration: 0.6,
   },
-  0.8
+  2.75
 );
+
 tl.from(
   ".box5",
   {
     y: 400,
   },
-  0.75
+  2.75
 );
+
 tl.from(
   ".box6",
   {
     y: 100,
   },
-  0.75
+  2.75
 );
 tl.from(
   ".box7",
   {
     x: -200,
   },
-  0.75
+  2.75
 );
 tl.from(
   ".box8",
   {
     y: 100,
   },
-  0.75
+  2.75
 );
 tl.from(
   ".box9",
   {
     x: 100,
   },
-  0.75
+  2.75
 );
 tl.from(
   ".arrow1",
   {
     autoAlpha: 0,
   },
-  0.75
+  2.75
 );
 tl.from(
   ".downArrow1",
   {
     scale: 0,
   },
-  0.75
+  2.75
 );
